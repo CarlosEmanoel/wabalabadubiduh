bd7c067c639ead54efdbad32c2b9c5b4344c5924
Carlos Emanoel <carlosemanoelmsousa@gmail.com>
2024-06-26 16:39:24 -0300

    a

--- performance.api/api/deploy.js
+++ performance.api/api/deploy.js
@@ -1,4 +1,45 @@
-import { spawn } from 'child_process';
+import { exec } from 'child_process';
+
+export const deploy = async (ctx) => {
+  try {
+    const { stdout, stderr } = await new Promise((resolve, reject) => {
+      exec('/home/deploy_performance.sh', (error, stdout, stderr) => {
+        if (error) {
+          if (error.code === 'ETIMEDOUT') {
+            console.warn('O script demorou muito para responder e foi interrompido por timeout.');
+            resolve({ stdout: 'Execução interrompida por timeout', stderr: '' });
+          } else {
+            reject(error);
+          }
+        } else {
+          resolve({ stdout, stderr });
+        }
+      });
+    });
+
+    if (stderr) {
+      console.error('Erro durante a execução do script:', stderr);
+      ctx.status = 500;
+      ctx.body = 'Erro durante a execução do script';
+    } else {
+      console.log('Saída do script:', stdout);
+      ctx.body = stdout;
+    }
+  } catch (error) {
+    if (error.code !== 'ETIMEDOUT') {
+      console.error('Erro ao executar o deploy:', error);
+      ctx.status = 500;
+      ctx.body = 'Erro ao executar o deploy : ' + error.message;
+    } else {
+      console.warn('Erro de timeout, tente novamente.');
+      ctx.status = 200;
+      ctx.body = 'Execução interrompida por timeout';
+    }
+  }
+};
+
+/* FUNCIONAL */
+/* import { spawn } from 'child_process';
 
 export const deploy = (ctx) => {
   const process = spawn('/home/deploy_performance.sh', { detached: true });
@@ -28,14 +69,18 @@ export const deploy = (ctx) => {
       ctx.body = 'Deploy falhou';
     }
   });
-};
+}; */
 
+
+
+
+/* PADRÃO */
 /* import { exec } from 'child_process';
 
 export const deploy = async (ctx) => {
   try {
     const { stdout, stderr } = await new Promise((resolve, reject) => {
-      exec('/home/deploy_performance.sh', { timeout: 0 }, (error, stdout, stderr) => {
+      exec('/home/deploy_performance.sh', (error, stdout, stderr) => {
         if (error) {
           reject(error);
         } else {
